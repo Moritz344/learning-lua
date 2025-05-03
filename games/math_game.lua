4552df6cd5641a976869ee78c3821e9263e3d364
Moritz344 <moritzmaier34@gmail.com>
2025-05-03 12:16:01 +0200

    made my first game: pong

--- games/math_game.lua
+++ games/math_game.lua
@@ -1,32 +1,68 @@
 
 run = true
+score = 0
 
 while run do
     
     number_1 = math.random(1, 11)
     number_2 = math.random(1, 11)
 
+    op = math.random()
+
+
     result = nil
+
+    function IncreaseScore(foo)
+      score = score + foo
+      print("Score:",score)
+      return score
+    end
     
     function add(n1,n2) 
-      formatted_aufgabe = string.format("%d + %d",n1,n2)
+      local formatted_aufgabe = string.format("%d + %d",n1,n2)
       print()
       print(formatted_aufgabe)
       result = n1+n2
     end
 
     function subtract(n1,n2)
-      formatted_aufgabe = string.format("%d - %d",n1,n2)
+      local formatted_aufgabe = string.format("%d - %d",n1,n2)
       print()
       print(formatted_aufgabe)
       result = n1-n2
     end
 
-    if math.random() > 0.6 then
-        add(number_1,number_2)
-    else
-      subtract(number_1,number_2)
+    function mal(n1,n2)
+      local formatted_aufgabe = string.format("%d * %d",n1,n2)
+      print()
+      print(formatted_aufgabe)
+      result = n1*n2
     end
+
+    function divide(n1,n2)
+      local formatted_aufgabe = string.format("%d / %d",n1,n2)
+      print()
+      print(formatted_aufgabe)
+      local factor = 10 ^ 2
+      result = n1/n2
+      local rounded_result = math.floor(result * factor + 0.5) / factor
+      result = rounded_result
+    end
+    
+    function decideAufgabe()
+        if op < 0.25 then
+          add(number_1,number_2)
+        elseif op < 0.5 then
+          subtract(number_1,number_2)
+        elseif op < 0.75 then
+          mal(number_1,number_2)
+        else
+          divide(number_1,number_2)
+        end
+    end
+
+    decideAufgabe()
+
     
     print("User: ")
     read_input = io.read("*number")
@@ -34,12 +70,15 @@ while run do
 
     function checkAnswer()
         if read_input == result then
+          score = IncreaseScore(1)
           print("Correct Answer.")
         else
+          score = 0
           print("Wrong Answer. The right answer was " .. result)
         end
     end
     checkAnswer()
+
 
     
     if read_input == nil then
--- games/pong/main.lua
+++ games/pong/main.lua
@@ -1,4 +1,109 @@
-function love.draw()
-    love.graphics.print("Hallo LÖVE!", 400, 300)
+
+-- TODO: sound + score
+
+function love.load()
+  player = {}
+  player.x = 200
+  player.y = 300
+  player.width = 10
+  player.height = 50
+  player.speed = 3
+
+  WindowMinHeight = 0
+  WindowMaxHeight = 550
+
+  player_2 = {}
+  player_2.x = 600
+  player_2.y = 300
+  player_2.speed = 3
+  
+  ball = {}
+  ball.x = 400
+  ball.y = 300
+  ball.speed_x = 3
+  ball.speed_y = 3
+  ball.size = 10
+
+
+  ball.checkCollision = function(ball,paddle)
+        return ball.x < paddle.x + 10 and
+               paddle.x < ball.x + ball.size and
+               ball.y < paddle.y + 50 and
+               paddle.y < ball.y + ball.size
+  end
+
 end
 
+function playerMovement()
+  if love.keyboard.isDown("w") then
+    player.y = player.y - player.speed
+  elseif love.keyboard.isDown("s") then
+    player.y = player.y + player.speed
+  end
+end
+
+function playerCollision()
+  if player.y < WindowMinHeight then
+    player.y = WindowMinHeight
+  elseif player.y > WindowMaxHeight then
+    player.y = WindowMaxHeight
+  end
+end
+
+
+function ballMovement()
+  ball.x = ball.x + ball.speed_x
+  ball.y = ball.y - ball.speed_y
+
+end
+
+function ballCollision()
+  if ball.y < WindowMinHeight then
+    ball.speed_y  = ball.speed_y * -1
+  elseif ball.y > WindowMaxHeight + 40 then
+    ball.speed_y = ball.speed_y * -1
+  end
+
+  if ball.x > 800 then
+    ball.speed_x = ball.speed_x * -1
+  elseif ball.x < 10 then
+    ball.speed_x = ball.speed_x * -1
+  end
+
+  if ball:checkCollision(player) then
+    ball.speed_x = ball.speed_x * -1
+  end
+  if ball:checkCollision(player_2) then
+    ball.speed_x = ball.speed_x * -1
+  end
+end
+
+function player_2KI()
+  if ball.y > player_2.y then
+    player_2.y = player_2.y + player_2.speed
+  elseif ball.y < player_2.y then
+    player_2.y = player_2.y - player_2.speed
+  end
+end
+
+
+function love.update(dt)
+    playerMovement()
+    playerCollision()
+
+    player_2KI()
+   
+    ballMovement()
+    ballCollision()
+
+end
+
+function love.draw()
+  -- players
+  love.graphics.rectangle("fill",player.x,player.y,player.width,player.height)
+  love.graphics.rectangle("fill",player_2.x,player_2.y,player.width,player.height)
+  --
+  -- ball
+  love.graphics.circle("fill",ball.x,ball.y,ball.size)
+
+end
